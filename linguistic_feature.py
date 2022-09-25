e6a5e9537da2ce9f3f646414a62171a33fecf213
Pau Korpajarasoontorn <dheerachote.k@gatech.edu>
2022-09-24 23:34:59 -0400

    Add more EDA

--- linguistic_feature.py
+++ linguistic_feature.py
@@ -14,5 +14,9 @@ def ling_feature(df):
     df_new['num_char'] = df_new['Ori_Review'].apply(len)
     df_new['char_per_word'] = df_new['num_char'] / df_new['num_word']
     first_sing = ['i', 'me', 'my', 'mine', 'myself']
+    first_pru = ['we', 'our', 'us', 'ours', 'ourselves']
+    third = ['he', 'she', 'they', 'his', 'her', 'their', 'hers', 'theirs', 'himself', 'herself', 'themselves']
     df_new['num_first_sing'] = df_new['Word_List_all'].apply(lambda x: count_pronoun(x, first_sing))
+    df_new['num_first_pru'] = df_new['Word_List_all'].apply(lambda x: count_pronoun(x, first_pru))
+    df_new['num_third'] = df_new['Word_List_all'].apply(lambda x: count_pronoun(x, third))
     return df_new
